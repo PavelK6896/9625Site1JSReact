ddf1dc85e7b31576c8e6576f87b18a0341f2486e
PavelK6896 <79208776896@yandex.ru>
2020-09-26 21:13:08 +0300

    "update style 4"

--- src/pages/Page.js
+++ src/pages/Page.js
@@ -14,7 +14,7 @@ export const Page = () => {
                     {/*<NavLink className="nav-link" to="/day"> дни </NavLink>*/}
                     <div className="nav-link"><a  href="https://pavelktrainer1.web.app"> тренер1 </a> слепой печати</div>
                     <div className="nav-link"><a  href="https://pavelktrainer2.web.app"> тренер2 </a> устного счета</div>
-                    <div className="nav-link"><NavLink  to="/generator"> генератор1 </NavLink>паролей</div>
+                    {/*<div className="nav-link"><NavLink  to="/generator"> генератор1 </NavLink>паролей</div>*/}
                 </div>
             </div>
             <Footer/>
