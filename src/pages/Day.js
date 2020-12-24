a13704592c85a3d5ad3d94f66942984f5947ee74
PavelK6896 <43148722+PavelK6896@users.noreply.github.com>
2020-12-24 18:53:23 +0300

    add select date

--- src/pages/Day.js
+++ src/pages/Day.js
@@ -11,7 +11,7 @@ export const Day = () => {
         let monthN = 0;
 
         const leapYear = function (y) {
-            return !(y & 3 || !(y % 25) && y & 15);
+            return !(y & 3 || !(y % 25) & y & 15);
         };
 
         let leapDay = 0;
@@ -37,12 +37,14 @@ export const Day = () => {
     const dataDay3 = dataDay1 - dataDay2;
 
 
+
     const [state, setState] = useState({
         check1: false,
         check2: false,
         dataDay1: dataDay1,
         dataDay2: dataDay2,
         dataDay3: dataDay3,
+        dataDay5: dataDay3,
         text1: 'с единицы',
         text2: 'седня включительно',
     })
@@ -58,6 +60,7 @@ export const Day = () => {
                         dataDay1: prevState.dataDay1 + 1,
                         dataDay2: prevState.dataDay2 + 1,
                         dataDay3: prevState.dataDay3 + 1,
+                        dataDay5: prevState.dataDay5 + 1,
                         text1: 'с нуля'
                     }
                 }
@@ -71,6 +74,7 @@ export const Day = () => {
                         dataDay1: prevState.dataDay1 - 1,
                         dataDay2: prevState.dataDay2 - 1,
                         dataDay3: prevState.dataDay3 - 1,
+                        dataDay5: prevState.dataDay5 - 1,
                         text1: 'с единицы'
                     }
                 }
@@ -89,6 +93,7 @@ export const Day = () => {
                         dataDay1: prevState.dataDay1 + 1,
                         dataDay2: prevState.dataDay2 + 1,
                         dataDay3: prevState.dataDay3 + 1,
+                        dataDay5: prevState.dataDay5 + 1,
                         text2: 'седня не включая',
                     }
                 }
@@ -102,11 +107,24 @@ export const Day = () => {
                         dataDay1: prevState.dataDay1 - 1,
                         dataDay2: prevState.dataDay2 - 1,
                         dataDay3: prevState.dataDay3 - 1,
+                        dataDay5: prevState.dataDay5 - 1,
                         text2: 'седня включительно',
                     }
                 }
             )
         }
+    }
+
+    const onChangeDate = (event) => {
+        const date = event.target.value.split('-')
+        const dataDay4 = dayN(+date[0], (date[1] - 1), +date[2]);
+        setState(
+            prevState => {
+                return {
+                    ...prevState,
+                    dataDay5:  (dataDay4 - dataDay2)
+                }
+            })
     }
 
     return (
@@ -128,7 +146,12 @@ export const Day = () => {
                     </p>
                     <p><input type={'checkbox'} onChange={onChangeDay}/>{state.text1}</p>
                     <p><input type={'checkbox'} onChange={onChangeToDay}/>{state.text2}</p>
-
+                    <p><input type={'date'} onChange={onChangeDate}/> date</p>
+                    <p>
+                        {state.dataDay5.toString().slice(0, state.dataDay5.toString().length - 3) +
+                        " " + state.dataDay5.toString().slice(state.dataDay5.toString().length - 3, state.dataDay5.toString().length)}
+                        &nbsp;день
+                    </p>
                 </div>
             </div>
             <Footer/>
