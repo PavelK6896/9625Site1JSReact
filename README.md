e03510c0ac6517ecefb118e6e2322ff235abf713
PavelK6896 <79208776896@yandex.ru>
2021-01-11 21:55:53 +0300

    add git page

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ## 9625Site1JSReact
 
-Site 1 [Site 1](https://pavelk.web.app)
+Site 1 [Site 1](https://pavelk.web.app) static server Firebase.
+
+Site 1 [Site 1](https://pavelk6896.github.io/9625Site1JSReact/) static server GitHub Pages.
+
---
+++ docs/precache-manifest.c24a574126576c2e1bf1dc6069860835.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31b95ee3b71547ea8c2a5e1c99addf64",
+    "url": "/9625Site1JSReact/index.html"
+  },
+  {
+    "revision": "6da3b0b36c6f48029b4b",
+    "url": "/9625Site1JSReact/static/css/main.1afd513a.chunk.css"
+  },
+  {
+    "revision": "c7d5a40ec8bebef2c8fe",
+    "url": "/9625Site1JSReact/static/js/2.4515b722.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/9625Site1JSReact/static/js/2.4515b722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6da3b0b36c6f48029b4b",
+    "url": "/9625Site1JSReact/static/js/main.74ee77b1.chunk.js"
+  },
+  {
+    "revision": "0db6c271518566ff1359",
+    "url": "/9625Site1JSReact/static/js/runtime-main.d52104ed.js"
+  }
+]);
